0d1f98af79cc2dc711ff552d6b644baaace2c82e
SonalKhare <sonalkhare.sk215@gmail.com>
2025-01-14 21:10:08 +1100

    app.js commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const https = require('https');
 const express = require('express');
-const bodyParser = require('body-parser');   
+const bodyParser = require('body-parser');
 const multer = require('multer');
 const helmet = require('helmet');
 const compression = require('compression');
@@ -18,33 +18,37 @@ const User = require('./models/user');
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'images')
+    cb(null, 'images');
   },
   filename: (req, file, cb) => {
     console.log(file);
     cb(null, new Date().toISOString() + '-' + file.originalname);
   },
-})
+});
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype==='image/png' || file.mimetype==='image/jpg' || file.mimetype==='image/jpeg') {
-    cb(null, true)
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
   } else {
-    cb(null, false)
+    cb(null, false);
   }
-  
-}
+};
+
 const MONGODB_URI =
-'mongodb+srv://rebelthyworld:Forgot%402692_@cluster0.mq49lnp.mongodb.net/shopDb?retryWrites=true&w=majority&appName=Cluster0';
+  'mongodb+srv://<mongodb_user>:<mongodb_user_password>@cluster0.q4mse.mongodb.net/shopDb?retryWrites=true&w=majority&appName=Cluster0';
 //const MONGODB_URI =
 //`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.mq49lnp.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
 const app = express();
 
 const store = new MongoDBStore({
   uri: MONGODB_URI,
-  collection: 'sessions', 
-})
-const csrfProtection = csrf()  //middleware
+  collection: 'sessions',
+});
+const csrfProtection = csrf(); //middleware
 
 const privateKey = fs.readFileSync('server.key');
 const certificate = fs.readFileSync('server.cert');
@@ -58,18 +62,18 @@ const { error } = require('console');
 
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, 'access.log'),
-  {flags: 'a'}
+  { flags: 'a' }
 );
 app.use(helmet());
 app.use(compression());
-app.use(morgan('combined', {stream: accessLogStream}));
+app.use(morgan('combined', { stream: accessLogStream }));
 
-
-
-app.use(bodyParser.urlencoded({extended: false}));  //use ful for x-www-form-urlencoded via <form>
-app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
-app.use(express.static(path.join(__dirname, "public")));
-app.use('/images', express.static(path.join(__dirname, "images")));
+app.use(bodyParser.urlencoded({ extended: false })); //use ful for x-www-form-urlencoded via <form>
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use(
   session({
@@ -87,24 +91,24 @@ app.use((req, res, next) => {
     return next();
   }
   User.findById(req.session.user._id)
-  .then(user=>{
-    if(!user){
-      return next();
-    }
-    req.user = user
-    next();
-  })
-  .catch(err=>{
-    next(new Error(err));
-  })
+    .then((user) => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
   // next();
-})
+});
 
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn,
-  res.locals.csrfToken = req.csrfToken(),
-  next();
-})
+  (res.locals.isAuthenticated = req.session.isLoggedIn),
+    (res.locals.csrfToken = req.csrfToken()),
+    next();
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -116,16 +120,16 @@ app.use(errorController.get404);
 app.use((error, req, res, next) => {
   console.log(error);
   res.redirect('/500');
-})
+});
 
-
-mongoose.connect(MONGODB_URI)
-.then(result => {
-  console.log('MONGOOSE CONNECTED');
-  // https
-  // .createServer({key: privateKey, cert: certificate}, app)
-  // .listen(3000)
-  // app.listen(procee.env.PORT || 3000);
-  app.listen(3000);
-})
-.catch(err=>console.log(err));
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    console.log('MONGOOSE CONNECTED');
+    // https
+    // .createServer({key: privateKey, cert: certificate}, app)
+    // .listen(3000)
+    // app.listen(procee.env.PORT || 3000);
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
